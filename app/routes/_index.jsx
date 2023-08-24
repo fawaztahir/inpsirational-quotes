85004e3b02ff90757f112861a51f1c787658f22c
Fawaz Tahir <fawaz.cb@gmail.com>
2023-08-24 14:34:48 +0500

    Index changes

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,39 +1,28 @@
 export const meta = () => {
-  return [
-    { title: "Inspire yourself with uplifting quotes" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+    return [
+        { title: "Inspire yourself with uplifting quotes" },
+        { name: "description", content: "Welcome to Remix!" },
+    ];
 };
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1 className="text-3xl font-bold underline">Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+        <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+            <div className="grid h-screen place-items-center">
+                <figure className="max-w-screen-md mx-auto text-center">
+                    <svg class="w-10 h-10 mx-auto mb-3 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+                        <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
+                    </svg>
+
+                    <blockquote className="text-2xl italic font-medium text-gray-900 dark:text-white">
+                        <p>Some quotation</p>
+                    </blockquote>
+                    <figcaption className="flex items-center justify-center mt-6 space-x-3">
+                        <cite class="font-medium text-gray-900 dark:text-white">Fawaz Tahir</cite>
+                    </figcaption>
+                </figure>
+
+            </div>
+        </div>
+    );
 }

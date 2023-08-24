d7b5a6104cd0a744bab412309f82202b4580a3bc
Fawaz Tahir <fawaz.cb@gmail.com>
2023-08-24 22:24:33 +0500

    addded analytics

--- app/root.jsx
+++ app/root.jsx
@@ -1,12 +1,13 @@
 import tailwindcss from "~/tailwind.css";
 
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
+    Links,
+    LiveReload,
+    Meta,
+    Outlet,
+    Scripts,
 } from "@remix-run/react";
+import { Analytics } from '@vercel/analytics/react';
 
 export const meta = () => {
     return [
@@ -16,23 +17,24 @@ export const meta = () => {
 };
 
 export const links = () => [
-  ...(tailwindcss ? [{ rel: "stylesheet", href: tailwindcss }] : []),
+    ...(tailwindcss ? [{ rel: "stylesheet", href: tailwindcss }] : []),
 ];
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body className="bg-black text-white overflow-hidden">
-        <Outlet />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width,initial-scale=1" />
+                <Meta />
+                <Links />
+            </head>
+            <body className="bg-black text-white overflow-hidden">
+                <Outlet />
+                <Scripts />
+                <LiveReload />
+                <Analytics />
+            </body>
+        </html>
+    );
 }

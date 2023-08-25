150e671223324c26cc1b2cd972214f916c4fcb2a
Fawaz Tahir <fawaz.cb@gmail.com>
2023-08-25 13:42:20 +0500

    added share on twitter

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -25,10 +25,18 @@ export default function Index() {
     const navigation = useNavigation();
     const isSubmitting = navigation.state === 'loading';
 
+    const shareOnTwitter = () => {
+        const text = `"${quote?.q}" — ${quote?.a}`;
+        const url = `https://twitter.com/intent/tweet?text=${text}&via=FawazTahir88&related=FawazTahir88`;
+        const positionLeft = (window.screen.width / 2) - 300;
+        const positionTop = (window.screen.height / 2) - 150;
+        window.open(url, 'share-on-twitter', `top=${positionTop},left=${positionLeft},width=600,height=300`);
+    }
+
     return (
         <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "2.5" }}>
             <div className="grid h-screen place-items-center">
-                <figure className="px-8  mx-auto text-center">
+                <figure className="px-8 mx-auto text-center transition-all" style={{opacity: isSubmitting ? 0.5 : 1, cursor: 'default'}}>
                     <svg className="w-10 h-10 mx-auto mb-3 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
                         <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
                     </svg>
@@ -37,9 +45,24 @@ export default function Index() {
                         <p>{quote?.q}</p>
                     </blockquote>
                     <figcaption className="flex items-center justify-center mt-6 space-x-3">
-                        <cite className="font-medium text-gray-900 dark:text-white">- {quote?.a}</cite>
+                        <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                            <cite className="font-medium text-gray-900 dark:text-white pr-3"> &mdash; {quote?.a}</cite>
+                            <div className="flex items-center">
+                                <button onClick={shareOnTwitter} title="Share on Twitter" className="p-3 mx-3 inline-block rounded-full" disabled={isSubmitting} style={{'background-color': '#1da1f2'}}>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="h-4 w-4"
+                                        fill="currentColor"
+                                        viewBox="0 0 24 24">
+                                        <path
+                                            d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
                     </figcaption>
                 </figure>
+
             </div>
 
             <footer className="sticky pb-12 bottom-0 text-center">
